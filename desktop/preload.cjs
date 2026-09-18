63ab8eff8e4f6ed36a9e26637772bdbe8edf41c0
mjaekim3 <mjae.kim3@gmail.com>
2026-09-18 18:47:22 +0900

    feat: connect secure Electron bridge and local preview; GREEN 14 tests

---
+++ desktop/preload.cjs
@@ -0,0 +1,4 @@
+const {contextBridge,ipcRenderer}=require('electron');
+const api={};
+for(const method of ['state','importPhotos','importArtwork','asset','pixelPreview','start','job','cancel','saveKey','export'])api[method]=async args=>{const result=await ipcRenderer.invoke('ongi:call',method,args);if(!result.ok)throw new Error(result.error);return result.data;};
+contextBridge.exposeInMainWorld('ongi',Object.freeze(api));
